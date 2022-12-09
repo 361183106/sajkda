c8b700aaf12398435932601a87175d579ab98160
院长 <32212075+yuanter@users.noreply.github.com>
2022-12-10 03:03:56 +0800

    新增获取config.sh文件环境
    新增获取config.sh文件环境

--- tools/ql_api.py
+++ tools/ql_api.py
@@ -69,7 +69,7 @@ def get_config_and_envs(name: str = None) -> list:
             #print(line.strip())
             exportinfo = line.strip()
             #去除注释#行
-            list_all = re.findall(r'^#.+?)', exportinfo,re.DOTALL)
+            list_all = re.findall(r'^#.+?', exportinfo,re.DOTALL)
             exportinfolist = []
             if len(list_all) > 1:
                 exportinfolist = exportinfo.split("#")
