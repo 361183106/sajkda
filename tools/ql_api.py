2b9fdc6837d6f63b3eca34dead4b05844ec0d53c
院长 <32212075+yuanter@users.noreply.github.com>
2022-12-10 01:21:14 +0800

    Update ql_api.py

--- tools/ql_api.py
+++ tools/ql_api.py
@@ -71,10 +71,11 @@ def get_config_and_envs(name: str = None) -> list:
             exportinfo = line.strip()
             #去除注释
             exportinfolist = exportinfo.split("\#")
-            print(exportinfolist)
+            
             if len(exportinfolist) > 1 :
                 exportinfo = exportinfolist[0].strip().replace("\"","").replace("\'","")
             list_all = re.findall(r'export[ ](.+?)', exportinfo,re.DOTALL)
+            print(exportinfo)
             for info in list_all:
                 tmp = info.split("=")
                 if len(tmp) > 1 :
