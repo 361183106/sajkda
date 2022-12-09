b12d721e61dce9bdc956ab856043f7b311da994b
院长 <32212075+yuanter@users.noreply.github.com>
2022-12-10 03:33:01 +0800

    Update ql_api.py

--- tools/ql_api.py
+++ tools/ql_api.py
@@ -67,19 +67,13 @@ def get_config_and_envs(name: str = None) -> list:
             if  not  line  :
                 break;
             #print(line.strip())
-            exportinfo = line.strip()
+            exportinfo = line.strip().replace("\"","").replace("\'","")
             #去除注释#行
             rm_str_list = re.findall(r'^#(.+?)', exportinfo,re.DOTALL)
             #print('rm_str_list数据：{}'.format(rm_str_list))
             exportinfolist = []
             if len(rm_str_list) == 1:
-                exportinfolist = exportinfo.split("#")
-                #print('exportinfolistd的长度：{}'.format(len(exportinfolist)))
-                if len(exportinfolist) < 2 :
-                    #去除首尾空格、单引号和双引号
-                    exportinfo = exportinfolist[0].strip().replace("\"","").replace("\'","")
-                else:
-                    exportinfo = ""
+                exportinfo = ""
             #list_all = re.findall(r'export[ ](.+?)', exportinfo,re.DOTALL)
             #print('exportinfo数据：{}'.format(exportinfo))
             #以空格分隔，数组0为export，数组1为后面需要的数据
@@ -87,7 +81,7 @@ def get_config_and_envs(name: str = None) -> list:
             #print('list_all数据：{}'.format(list_all))
             if len(list_all) > 1:
                 #以=分割，查找需要的环境名字
-                tmp = list_all[1].replace("\"","").replace("\'","").split("=")
+                tmp = list_all[1].split("=")
                 if len(tmp) > 1:
                     
                     info = tmp[0]
