9094048cb463bb4b6e2e019a5063ed524444bfd0
院长 <32212075+yuanter@users.noreply.github.com>
2022-12-14 00:03:45 +0800

    蒙牛呛奶
    蒙牛呛奶

--- mengniu.py
+++ mengniu.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python3
+# -- coding: utf-8 --
+# -------------------------------
+# @Author : github@yuanter https://github.com/yuanter
+# @Time : 2022/8/10 13:23
+# -------------------------------
+# cron "55 59 8 * * *" script-path=xxx.py,tag=匹配cron用
+# const $ = new Env('蒙牛呛奶');
+"""
+蒙牛呛奶 微信小程序入口:蒙牛世界杯 抓 领奖记录请求中的 Authorisation
+1. 脚本仅供学习交流使用, 请在下载后24h内删除
+2. 需要第三方库 requests 支持 命令行安装 pip3 install requests或者根据自己环境自行安装
+3. 环境变量说明 mengniuCK(必需)  自行新建环境变量添加
+    mengniuCK 为你的Authorisation值  多账号使用&或者#隔开
+"""
 import base64
 import datetime
 import hashlib
@@ -11,6 +26,7 @@ import time
 import requests
 from Crypto.Cipher import DES
 from Crypto.Util.Padding import pad, unpad
+from tools.ql_api import get_cookie
 
 
 def printf(text):
@@ -73,17 +89,20 @@ def getRKSign(timestamp, nonce):
     md5Str = f'clientKey={clientKey}&clientSecret={clientSecret}&nonce={nonce}&timestamp={timestamp}'
     return hashlib.md5(md5Str.encode('utf-8')).hexdigest().upper()
 
-def getRk():
+def getRk(domain):
     timestamp = getTimestamp()
     nonce = generate_random_str(16)
     sign = getRKSign(timestamp, nonce)
     url = f'{domain}/mengniu-world-cup/mp/api/user/baseInfo?timestamp={timestamp}&nonce={nonce}&signature={sign}'
-    res = requests.get(url=url, headers=head).json()
-    printf(res)
+    
     try:
+        res = requests.get(url=url, headers=head).json()
+        printf(res)
         return res['data']['rk']
     except Exception:
+        #printf('获取账号rk失败，该token已经触发风控机制，请重新抓包获取新token')
         raise Exception('获取账号rk失败，该token已经触发风控机制，请重新抓包获取新token')
+    
 
 def getMilkSign(requestId, timestamp, rk):
     md5Str = f'requestId={requestId}&timestamp={timestamp}&key={rk}'
@@ -101,6 +120,7 @@ def skillMilk(rk, jsonId):
     head['requestId'] = requestId
     res = requests.get(url=url, headers=head, timeout=10).text
     printf(res)
+    
 
 def isStart():
     current_time = getTimestamp()
@@ -109,7 +129,23 @@ def isStart():
     else:
         return False
 
-if __name__ == '__main__':
+
+def start(token):
+    # config={
+    # "token": token,
+    # "clientKey": "FIDBFh4U65amvCDIlvE92WECR8txa48K",
+    # "clientSecret": "qzEKyCTxQdaquxm5u2OJKB3bVTie4f9qHTQIDTIGxCc88egeIAyJ6QXQeow8whvU",
+    # "updateUrl": "/mp/api/user/seckill/ghg3/dff/dd2/dsfs2/e21d/vddc",
+    # "threadNumber": 200,
+    # "preTime": 2000,
+    # "desKey": "pZN8^thwwfKl8^oz",
+    # "domain": "https://mengniu-apig.xiaoyisz.com",
+    # "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.30(0x18001e31) NetType/WIFI Language/zh_CN",
+    # "Referer": "https://servicewechat.com/wx8e45b2134cbeddff/62/page-frame.html"
+    # }
+
+
+    config = {}
     try:
         with open('./mengniu_config.json', 'r') as c:
             rdConfigStr = c.read()
@@ -127,7 +163,9 @@ if __name__ == '__main__':
     '''
     token是小程序包的请求头的Authorization: 
     '''
-    token = config['token']
+    #修改本处token的加载，优先环境，再json文件
+    if token == "":
+        token = config['token']
     desKey = config['desKey']
     clientKey = config['clientKey']
     clientSecret = config['clientSecret']
@@ -141,11 +179,20 @@ if __name__ == '__main__':
         'content-type': 'application/json',
         'Authorization': token
     }
+    # print(token)
+    # print(desKey)
+    # print(clientKey)
+    # print(clientSecret)
+    # print(updateUrl)
+    # print(domain)
+
+
+
 
     '''
     账号的rk 自动获取
     '''
-    rk = getRk()
+    rk = getRk(domain)
     '''
     抢多少次最大24 多了触发风控机制 导致无法获取rk 所有接口返回{"code":500,"message":"非领奶时间"} 触发风控机制后需要更新toekn才能恢复正常
     '''
@@ -179,3 +226,56 @@ if __name__ == '__main__':
             printf("等待开始...")
         time.sleep(0.1)
     os.system('pause')
+
+
+
+
+if __name__ == '__main__':
+    #初始参数
+    domain = ""
+    token = ""
+    desKey = ""
+    clientKey = ""
+    clientSecret = ""
+    updateUrl = ""
+    head = {}
+
+
+    l = []
+    user_map = []
+    cklist = get_cookie("mengniuCK")
+    for i in range(len(cklist)):
+        #以#、&、换行符分割开的ck
+        split1 = cklist[i].split("#")
+        split2 = cklist[i].split("&")
+        split3 = cklist[i].split("\n")
+        if len(split1)>1:
+            for j in range(len(split1)):
+                user_map.append(split1[j])
+        elif len(split2)>1:
+            for j in range(len(split2)):
+                user_map.append(split2[j])
+        elif len(split3)>1:
+            for j in range(len(split3)):
+                user_map.append(split3[j])
+        else:
+            if cklist[i] != "":
+                user_map.append(cklist[i])
+
+
+    for i in range(len(user_map)):
+        token=""
+        token = user_map[i]
+        print('开始执行第{}个账号：{}'.format((i+1),token))
+        if token == "":
+            print("当前账号未填写token 跳过")
+            print("\n")
+            continue
+        p = threading.Thread(target=start,args=(token,))
+        l.append(p)
+        p.start()
+        print("\n")
+    for i in l:
+        i.join()
+
+
